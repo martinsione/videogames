295dbc90c76bb6067c93e3ce3b4ab76a94554747
Martin Sione <martinsione03@gmail.com>
2022-02-28 18:27:06 -0300

    finish videogame controller

--- api/src/controllers/videogame.controller.ts
+++ api/src/controllers/videogame.controller.ts
@@ -1,40 +1,67 @@
 import axios from "axios";
 import type { Request, Response } from "express";
-import { videogameModel } from "../db";
+import { genreModel, videogameModel } from "../db";
 import type { IGenre, IPlatform, IVideogame } from "../types";
 
 const API = "https://api.rawg.io/api/games";
 const { API_KEY } = process.env;
 
-const getVideogameByIdFromApi = async (id: string): Promise<IVideogame> => {
+const getVideogameByIdFromApi = async (id: string) => {
   const { data }: { data: IVideogame } = await axios.get(`${API}/${id}`, {
     params: { key: API_KEY },
   });
-  return data;
+
+  return {
+    id: data.id,
+    name: data.name,
+    description: data.description,
+    genres: data.genres.map((genre: IGenre) => genre.name),
+    release: data.released,
+    rating: data.rating,
+    platforms: data.platforms.map(
+      (platform: IPlatform) => platform.platform.name
+    ),
+    image: data.background_image,
+  };
+};
+
+const getVideogameByIdFromDb = async (id: string) => {
+  const videogame = await videogameModel
+    .findByPk(id, {
+      include: [
+        {
+          model: genreModel,
+          as: "genres",
+          attributes: ["name"],
+          through: { attributes: [] },
+        },
+      ],
+    })
+    .then((videogame: any) => videogame.toJSON());
+
+  videogame.genres = videogame.genres.map((genre: IGenre) => genre.name);
+  return videogame;
+};
+
+const isUUID = (id: string): boolean => {
+  const regex =
+    /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+  return regex.test(id);
 };
 
 export const getVideogameById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  if (!id) {
-    return res.status(400).json({ message: "Missing id" });
-  }
+  if (!id) return res.status(400).json({ message: "Missing id" });
 
   try {
-    const data = await getVideogameByIdFromApi(id);
-    res.json({
-      id: data.id,
-      name: data.name,
-      description: data.description,
-      genre: data.genres.map((genre: IGenre) => genre.name),
-      release: data.released,
-      rating: data.rating,
-      platforms: data.platforms.map(
-        (platform: IPlatform) => platform.platform.name
-      ),
-      image: data.background_image,
-    });
+    if (isUUID(id)) {
+      const videogame = await getVideogameByIdFromDb(id);
+      return res.json(videogame);
+    }
+    const videogame = await getVideogameByIdFromApi(id);
+    res.json(videogame);
   } catch (e) {
-    res.status(500).json(e);
+    return res.status(500).json(e);
   }
 };
 
@@ -68,10 +95,11 @@ export const addVideogame = async (req: Request, res: Response) => {
   const [videogame, created] = await videogameModel.findOrCreate({
     where: { ...game },
   });
-  const genre = await videogame.setGenres(genres);
 
-  if (!created) {
-    return res.status(400).json({ message: "Videogame already exists" });
+  if (created) {
+    await videogame.addGenres(genres);
   }
-  res.json(videogame);
+
+  const videogameWithGenres = await getVideogameByIdFromDb(videogame.id);
+  res.json({ videogame: videogameWithGenres, created: created });
 };
