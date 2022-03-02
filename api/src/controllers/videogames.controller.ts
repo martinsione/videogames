d36048a2eb613fc27287571b71e19ab885e7f9d4
Martin Sione <martinsione03@gmail.com>
2022-03-02 17:47:56 -0300

    Fetch 80 games for pagination

--- api/src/controllers/videogames.controller.ts
+++ api/src/controllers/videogames.controller.ts
@@ -8,15 +8,33 @@ const API = "https://api.rawg.io/api/games";
 const { API_KEY } = process.env;
 
 const getFromApi = async (name: any) => {
-  const { data }: { data: IVideogames } = await axios.get(API, {
-    params: { key: API_KEY, page_size: 90, search: name || "" },
-  });
-  return data.results.map((game: IVideogame) => ({
-    id: game.id,
-    name: game.name,
-    image: game.background_image,
-    genres: game.genres.map((genre: IGenre) => genre.name),
-  }));
+  type Data = { data: IVideogames };
+  if (name) {
+    return axios
+      .get(API, { params: { key: API_KEY, page_size: 15, search: name } })
+      .then((response: Data) => {
+        return response.data.results.map((game: IVideogame) => ({
+          id: game.id,
+          name: game.name,
+          image: game.background_image,
+          genres: game.genres.map((genre: IGenre) => genre.name),
+        }));
+      });
+  } else {
+    return Promise.all([
+      axios.get(API, { params: { key: API_KEY, page_size: 40 } }),
+      axios.get(API, { params: { key: API_KEY, page_size: 40, page: 2 } }),
+    ]).then(([page1, page2]: Data[]) => {
+      // Concate page 1 and page 2
+      const data = page1.data.results.concat(page2.data.results);
+      return data.map((game: IVideogame) => ({
+        id: game.id,
+        name: game.name,
+        image: game.background_image,
+        genres: game.genres.map((genre: IGenre) => genre.name),
+      }));
+    });
+  }
 };
 
 const getFromDb = async (name: any) => {
