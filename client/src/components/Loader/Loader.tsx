097ecdc9507dce541e869d6a2ca4163f35c8e9c7
Martin Sione <martinsione03@gmail.com>
2022-03-04 10:58:29 -0300

    Change loader

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,7 +1,10 @@
 import styles from "./Loader.module.css";
 
 export const Loader = () => (
-  <div className={styles.center}>
-    <div className={styles.planet}></div>
+  <div className={styles.center} aria-label="Loading…">
+    <div className={styles.loaderContainer} aria-hidden="true">
+      <div className={styles.loader}></div>
+      <div className={styles.loader}></div>
+    </div>
   </div>
 );
