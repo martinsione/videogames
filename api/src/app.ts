236a259ff14cabe1c1c7991a28134e4a11fb1657
Martin Sione <martinsione03@gmail.com>
2022-03-05 12:24:04 -0300

    Prepare for deploy

--- api/src/app.ts
+++ api/src/app.ts
@@ -10,7 +10,7 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
